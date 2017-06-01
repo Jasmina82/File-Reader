6690f23e7355689917aa132bf7734c4044912e7f
Dejan Sipovac <dejansipovac@hotmail.com>
2017-06-02 00:18:48 +0200

    majne metode
    nesto na frku, metodom  
    Keep
    It
    Simple
    Stupid

---
+++ src/Metode.java
@@ -0,0 +1,46 @@
+package testneKlase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class JasminaKarakteristikeFajla {
+
+	public static void main(String[] args) {
+		String wholeText;
+		//Kreiramo File i Scanner objekat za citanje
+		File file = new File("tekst.txt");
+		try {
+			Scanner input = new Scanner(file);
+			
+			//Ucitavanje podataka u string
+			StringBuilder build=new StringBuilder();
+			StringBuffer buff=new StringBuffer(build);
+			
+			while(input.hasNext()){
+				buff.append(input.nextLine());
+			}
+			
+			//Dodijelimo zbog preglednosti cijeli tekst u string
+			wholeText=buff.toString();
+			
+			System.out.println(wholeText);
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File ne postoji!");
+		}
+		
+		
+		
+		
+
+	}
+
+	// Metoda za broj rijeci u file-u
+	public static int brojRijeci() {
+		return 1;
+	}
+  public static int brojSlova() {
+		return 1;
+	}
+  }
